abb52f4c69896ac2545ab8a7eee96d0f5a7bf41a
lucrasn <lucrasnobrega.araujo@gmail.com>
2025-02-22 17:07:34 -0300

    metodos: mediaNotas; frewFormated; determinarStatus

--- src/Main.java
+++ src/Main.java
@@ -23,9 +23,15 @@ public class Main {
                         System.out.print("Digite o nome da materia: ");
                         String materia = sc.nextLine();
                         materia = materiaFormated(materia);
-                        System.out.println(materia);
 
-                        // System.out.print("Digite a nota da");
+                        System.out.print("Digite a nota da 1ª unidade: ");
+                        float nt1 = sc.nextFloat();
+
+                        System.out.print("Digite a nota da 2ª unidade: ");
+                        float nt2 = sc.nextFloat();
+
+                        System.out.printf("Qual foi a sua frequência em %s? [0 a 100] ", materia);
+                        float freq = sc.nextFloat();
 
                         break;
                     case 2:
@@ -59,7 +65,7 @@ public class Main {
     public static String materiaFormated(String nm) {
         nm = nm.toLowerCase();
         String[] nmSeparado = nm.split(" ");
-        /*Ele não tava deixando a letra maiúscula antes por que quando a gente faz "for (String name : nmSeparado)"
+        /*Ele não tava deixando a letra maiúscula antes porque quando a gente faz "for (String name : nmSeparado)"
         * esse "name" não vai ser de fato cada elemento do array, mas sim uma cópia.
         * Então a gente até botava a primeira letra maiúscula mas a informação se perdia porque a gente não tava explicitamente
         * alterando os elementos do array! Por isso tive q mudar o raciocínio um pouco.*/
@@ -70,4 +76,21 @@ public class Main {
         }
         return String.join(" ", nmSeparado);
     }
+
+    // Metodo para calcula a média da disciplina
+    public static float mediaNotas(float n1, float n2) {
+        return (n1 + n2) / 2;
+    }
+
+    // Metodo para formatar a frequência
+//    public static float freqFormated(float f) {
+//
+//    }
+
+    // Metodo para determinar o status da disciplina
+//    public static String determinarStatus(float media, float f) {
+//        if (f >= 75) {
+//
+//        }
+//    }
 }
