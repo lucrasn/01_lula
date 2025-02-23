8c602306b8f4a425854f9a6102ca024e14f5dd18
lucrasn <lucrasnobrega.araujo@gmail.com>
2025-02-22 23:30:51 -0300

    Fechando o objeto Scanner

--- src/Main.java
+++ src/Main.java
@@ -68,6 +68,7 @@ public class Main {
                     System.out.println("Opção inválida, tente novamente!\n");
             }
         }
+        sc.close();
     }
 
     // Metodo para escrever a menu
