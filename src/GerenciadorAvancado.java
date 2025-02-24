e43ed46d3f4fcd51aaa835b8757e8cb63d6fdea4
lucrasn <lucrasnobrega.araujo@gmail.com>
2025-02-24 03:53:34 -0300

    Parte dois do exercícios: buscaEspecifica recursiva feita

--- src/GerenciadorAvancado.java
+++ src/GerenciadorAvancado.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 // Programa para Gerenciar as Disciplinas Avançado
-public class GerenciadorAvancado {
+public class GerenciadorAvancado { // A verdade é que eu queria que GerenciadorAvancado herdasse os coiso de GerenciadorGeral, mas não vou fazer isso porque não estudei muito sobre
     public static void main(String[] args) {
         String[][] boletim = new String[1000][5];
         Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
         boolean flag = true;
         int count = 0;
 
@@ -18,24 +20,32 @@ public class GerenciadorAvancado {
                 case 1:
                     System.out.print("\nDigite o nome da materia: "); // nome da materia
                     String materia = sc.nextLine();
-                    boletim[count][0] = materia = materiaFormated(materia);
+                    materia = formatName(materia);
 
-                    System.out.print("Digite a nota da 1ª unidade: "); // nota 1
-                    float nt1 = sc.nextFloat();
-                    boletim[count][1] = String.format("%.1f", nt1);
+                    // Percebi a possibilidade da pessoa tentar adicionar uma materia que ja existe
+                    if (count == 0 || !disciplinaExist(boletim, materia, count).exist()) {
+                        boletim[count][0] = materia;
+                        System.out.print("Digite a nota da 1ª unidade: "); // nota 1
+                        float nt1 = sc.nextFloat();
+                        boletim[count][1] = String.format("%.1f", nt1);
 
-                    System.out.print("Digite a nota da 2ª unidade: "); // nota 2
-                    float nt2 = sc.nextFloat();
-                    boletim[count][2] = String.format("%.1f", nt2);
+                        System.out.print("Digite a nota da 2ª unidade: "); // nota 2
+                        float nt2 = sc.nextFloat();
+                        boletim[count][2] = String.format("%.1f", nt2);
 
-                    System.out.printf("Qual foi a sua frequência em %s? [0 a 100] ", materia); // frequência
-                    float freq = sc.nextFloat();
-                    boletim[count][3] = String.format("%.0f", freq);
+                        System.out.printf("Qual foi a sua frequência em %s? [0 a 100] ", materia); // frequência
+                        float freq = sc.nextFloat();
+                        boletim[count][3] = String.format("%.0f", freq);
 
-                    System.out.print("\n");
-                    String status = determinarStatus(mediaNotas(nt1, nt2), freq); // Status
-                    boletim[count][4] = status;
-                    count++;
+                        System.out.print("\n");
+
+                        String status = determinarStatus(mediaNotas(nt1, nt2), freq); // Status
+                        boletim[count][4] = status;
+
+                        count++;
+                    } else {
+                        System.out.println("\nDisciplina já cadastrada! Tente a opção 2\n");
+                    }
                     break;
                 case 2:
                     if (count == 0) {
@@ -43,8 +53,8 @@ public class GerenciadorAvancado {
                     } else {
                         System.out.print("Digite o nome da matéria a ser consultada: ");
                         String consulta = sc.nextLine();
-                        consulta = materiaFormated(consulta);
-                        buscaEspecifica(boletim, count, consulta);
+                        consulta = formatName(consulta);
+                        System.out.println(buscaEspecifica(boletim, consulta, count));
                     }
                     break;
                 case 3:
@@ -73,7 +83,7 @@ public class GerenciadorAvancado {
     }
 
     // Metodo para formatar o nome da materia
-    public static String materiaFormated(String nm) {
+    public static String formatName(String nm) {
         nm = nm.toLowerCase();
         String[] nmSeparado = nm.split(" ");
         for (int i = 0; i < nmSeparado.length; i++) {
@@ -105,34 +115,43 @@ public class GerenciadorAvancado {
     }
 
     // Metodo de formatação de como será mostrada as materias
-    public static void formatMaterias(String[][] boletim, int i) {
-        System.out.println("\nMatéria: " + boletim[i][0]);
-        System.out.println("Nota 1: " + boletim[i][1]);
-        System.out.println("Nota 2: " + boletim[i][2]);
-        System.out.println("Frequência: " + boletim[i][3] + "%");
-        System.out.println("Status: " + boletim[i][4] + "\n");
+    public static String formatView(String[][] boletim, int i) {
+        return "\nMatéria: " + boletim[i][0] + "\nNota 1: " + boletim[i][1] + "\nNota 2: " + boletim[i][2] +
+                "\nFrequência: " + boletim[i][3] + "%" + "\nStatus: " + boletim[i][4] + "\n\n";
+    }
+
+    public record Verify(boolean exist, int indice) {}
+
+    // Metodo para verificar se a disciplina ja existe
+    public static Verify disciplinaExist(String[][] boletim , String consulta, int count) {
+        return disciplinaExist(boletim, consulta, count, 0);
+    }
+
+    // Metodo para verificar se a disciplina ja existe RECURSIVA
+    public static Verify disciplinaExist(String[][] boletim , String consulta, int count, int i) {
+        if (count == i) {
+            return new Verify(false, count);
+        } else if (boletim[i][0].equalsIgnoreCase(consulta)) {
+            return new Verify(true, i);
+        } else {
+            return disciplinaExist(boletim, consulta, count, i + 1);
+        }
     }
 
     // Metodo para buscar uma disciplina especifica
-    public static void buscaEspecifica(String[][] boletim, int count, String consulta) {
-        boolean cadastrada = false;
-
-        for (int i = 0; i < count; i++) {
-            if (boletim[i][0].equalsIgnoreCase(consulta)) {
-                formatMaterias(boletim, i);
-                cadastrada = true;
-            }
-        }
-
-        if (!cadastrada) {
-            System.out.println("Disciplina não encontrada");
+    public static String buscaEspecifica(String[][] boletim , String consulta, int count) {
+        Verify vy = disciplinaExist(boletim, consulta, count);
+        if (!vy.exist()) {
+            return "\nDisciplina não cadastrada!\n";
+        } else {
+            return formatView(boletim, vy.indice());
         }
     }
 
     // Metodo para buscar todas as disciplinas
     public static void buscaAll(String[][] boletim, int count) {
         for (int i = 0; i < count; i++) {
-            formatMaterias(boletim, i);
+            System.out.print(formatView(boletim, i));
         }
     }
 }
